af68f294573015046a4486e1d3e25de89317cc50
Francisco Giráldez Gámez <fgiraldez@anybotics.com>
2020-08-30 11:20:14 +0000

    Merge branch 'fix/add_test_cov_fgg' into 'master'
    [motion_control] Add gtest coverage for some pkgs
    
    GitOrigin-RevId: 9938a6a16d6b9121a546cfbd017da438d6c7990d

---
+++ test/TestOsqp.cpp
@@ -0,0 +1,11 @@
+/*!
+ * @file        TestAnymalCtrlLearnedGaits.cpp
+ * @authors     Francisco Giraldez (ANYbotics)
+ * @brief       Tests for OSQP.
+ */
+
+#include <gtest/gtest.h>
+
+TEST(TestOsqp, dummyTest) {  // NOLINT
+ASSERT_TRUE(true);
+}
